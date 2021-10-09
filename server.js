731061fa52c5b8f82c50fca36ddc0b154e1efc70
George (Mac) Pence <71496139+georgepence@users.noreply.github.com>
2021-10-09 14:49:42 -0400

    CS361 Assignment 3
    Show test message from REST API Call

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.get('/imageSearch', ((req, res) => {
+  
+    const url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyAqsEc83ZtQM-aUDoxqyUSKZ4nK6gyXRAg&cx=1d51ed3d0c23e53c2&q=lectures&searchType=image'
+    req = https.request(url, res => {
+      console.log(`statusCode: ${res.statusCode}`)
+      
+      console.log(res.JSON)
+    })
+    
+    req.on('error', err => console.error(err))
+   req.end()
+  
+}))
 
 app.get('*', ((req, res) => {
-  res.send('Chico is hungry')
+  res.send('Message from CS361')
 }));
 
 app.listen(port, ()=> console.log(`server started on port ${port}.`));
