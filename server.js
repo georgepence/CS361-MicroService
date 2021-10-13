c8d750e517245cca28af0562949460e8cd8abf0a
George (Mac) Pence <71496139+georgepence@users.noreply.github.com>
2021-10-13 14:53:58 -0400

    Testing route imageSearch (3)

--- server.js
+++ server.js
@@ -12,9 +12,55 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.get('/getImage'), ((req, res) => {
+app.get('/getImage', ((req, res) => {
+  
+  const url = 'https://web.engr.oregonstate.edu/~penceg/roscoe-memorial/images/gate.shower?width=450';
+  console.log("Req send = ", req.query.send)
+  
+  if (!req.query.send || req.query.send === "link") {
+    // URL of the image
+    res.send(res.json(url))
+    
+  } else {
+    try {
+      console.log("I'm here now!!!")
+      res.sendFile(`${__dirname}/images/tennis.jpg`)
+      console.log('sent!')
+    } catch (error) {
+      console.error(error);
+    }
 
-})
+  }
+
+}))
+
+// ============================================================================
+app.get('/chico', ((req, res) => {
+  console.log("Found Chico!!")
+  res.send(JSON.stringify("I am a small dog"))
+  
+  async function Chico() {
+    // URL of the image
+    const url = 'https://web.engr.oregonstate.edu/~penceg/roscoe-memorial/images/gate.jpeg?width=450';
+    
+    https.get(url,(res) => {
+      console.log("Status = ", res.statusCode)
+      // Image will be stored at this path
+      const path = `${__dirname}/mac-app/images/cave.jpeg`;
+      console.log("path = ", path)
+      const filePath = fs.createWriteStream(path);
+      res.pipe(filePath);
+      filePath.on('finish',() => {
+        filePath.close();
+        console.log('Download Completed');
+      })
+    })
+  }
+  
+  Chico().then((res) => console.log("yahoo!!", res)).catch(err => console.log(err));
+  
+}))
+// ============================================================================
 
 app.get('/imageSearch', ((req, res) => {
   
